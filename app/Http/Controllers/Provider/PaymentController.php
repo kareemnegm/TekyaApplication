3ce3569bc1d9db84500f72e43705d347a828185f
kareem negm <kareem.f.nagm@gmail.com>
2022-08-21 22:31:22 +0200

    updates

--- app/Http/Controllers/Provider/PaymentController.php
+++ app/Http/Controllers/Provider/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Provider;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PaymentRequest;
 use App\Http\Resources\PaymentResource;
 use App\Models\PaymentOption;
 use Illuminate\Http\Request;
@@ -27,7 +28,7 @@ class PaymentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PaymentRequest $request)
     {
         PaymentOption::create([
             'name' => $request->name
