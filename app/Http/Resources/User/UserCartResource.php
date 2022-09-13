1670fa8f97de508feb67bdd15006b85385808e97
Anwarsaeed1 <anwarsaeed1@yahoo.com>
2022-09-13 12:49:12 +0200

    Modfied

--- app/Http/Resources/User/UserCartResource.php
+++ app/Http/Resources/User/UserCartResource.php
@@ -3,7 +3,9 @@
 namespace App\Http\Resources\User;
 
 use App\Http\Resources\ImageResource;
+use App\Models\Cart;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class UserCartResource extends JsonResource
 {
@@ -15,14 +17,17 @@ class UserCartResource extends JsonResource
      */
     public function toArray($request)
     {
+        $cart_id = Auth::user()->cart->id;
+        $cart  = Cart::findOrFail($cart_id);
+ 
         return [
             'id' => $this->id,
             'shop_name' => $this->shop_name,
             'shop_logo' => new ImageResource($this->getFirstMedia('shop_logo')) ?? null,
             'shop_cover' => new ImageResource($this->getFirstMedia('shop_cover')) ?? null,
-            'total_products'=>count($this->productsCarts),
+            'total_products'=>$this->productsCarts()->where('cart_id',$cart->id)->count(),
             'total_price'=>$this->productsCarts->sum('price'),
-            'products' => CartProductResource::collection($this->productsCarts)
+            'products' => CartProductResource::collection($this->productsCarts()->where('cart_id',$cart->id)->get())
 
         ];
     }
