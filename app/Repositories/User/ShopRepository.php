f6a6983a8039644e77232eb07cb3bf8c652c4823
kareem negm <57332546+kareemnegm@users.noreply.github.com>
2022-09-28 17:06:51 +0200

    nearest branches

--- app/Repositories/User/ShopRepository.php
+++ app/Repositories/User/ShopRepository.php
@@ -90,10 +90,11 @@ class ShopRepository extends Controller implements ShopInrerface
      */
     public function getShopDetails($request)
     {
-        $latitude = $request->latitude ? $request->latitude : 30.012537910528884;
-        $longitude = $request->longitude ? $request->longitude : 31.290307;
+        $latitude = $request->latitude?$request->latitude:30.012537910528884;
+        $longitude = $request->longitude?$request->longitude:31.290307;
         $q = providerShopBranch::ByDistance($latitude, $longitude, array($request->shop_id))->first();
         return $q;
+
     }
 
 
@@ -105,12 +106,12 @@ class ShopRepository extends Controller implements ShopInrerface
      */
     public function getShopBranches($request)
     {
-        $latitude = $request->latitude ? $request->latitude : 30.012537910528884;
-        $longitude = $request->longitude ? $request->longitude : 31.290307;
+
         $shop = ProviderShopDetails::findOrFail($request->shop_id);
-        $q = providerShopBranch::ByDistance($latitude, $longitude, array($shop->id))->get();
+        $q = $shop->branches();
 
+        $branches = $q->orderBy('id', 'ASC')->get();
 
-        return $q;
+        return $branches;
     }
 }
