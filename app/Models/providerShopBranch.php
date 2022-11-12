ed3ad44400e528b63fab9d3cb356a6fdcd0291e7
Anwarsaeed1 <anwarsaeed1@yahoo.com>
2022-11-12 03:02:14 +0200

    Modfied Singel Product

--- app/Models/providerShopBranch.php
+++ app/Models/providerShopBranch.php
@@ -141,8 +141,7 @@ class providerShopBranch extends Model
         
        return  providerShopBranch::where('shop_id',$shop_id)->select(DB::raw("$haversine AS distance, id as id , name as name,shop_id as shop_id"),'latitude','longitude')
                 ->orderby("distance", "asc")
-                ->having("distance", "<=", $distance)
-                ->first();
+                ->having("distance", "<=", $distance);
         
     }
 
