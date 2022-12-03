fe27f569beea4c2df7a3ee5df4378b921d7e3037
Anwarsaeed1 <anwarsaeed1@yahoo.com>
2022-12-03 19:37:55 +0200

    Modfied Order Type In Orders

--- app/Http/Resources/Provider/ProviderPlacedOrdersDetailsResource.php
+++ app/Http/Resources/Provider/ProviderPlacedOrdersDetailsResource.php
@@ -21,7 +21,7 @@ class ProviderPlacedOrdersDetailsResource extends JsonResource
             'order_id'=>$this->order_id,
             'order_shop_id' => $this->id,
             "order_number" => $this->order->order_number,
-            'delivery_time' => Carbon::parse($this->order->date_order_placed)->format('l M-Y g:i A'),
+            'order_placed_date' => Carbon::parse($this->order->date_order_placed)->format('l M-Y g:i A'),
             'order_items' => new ProviderOrderItemResource($this),
             'coupon' => $this->invoice->coupon_id,
             'invoice_order' => new OrderInvoiceResource($this->invoice),
